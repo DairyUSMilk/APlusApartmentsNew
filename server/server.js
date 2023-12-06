247eb1c4c70ef514664532bcc81a5cc95235c78d
George Sheng <gsheng112@gmail.com>
2023-12-05 23:32:04 -0500

    Added user db functions

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
+import * as users from "./src/data/users.js";
 import { typeDefs } from "./src/controllers/typeDefs.js";
 import { resolvers } from "./src/controllers/resolvers.js";
 
