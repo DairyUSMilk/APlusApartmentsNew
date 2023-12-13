e0d04b0d8b547c6be7c9265e0417cc1a0b653e75
Guizhi Xu <113871493+GarethTorres@users.noreply.github.com>
2023-12-13 06:29:41 -1000

    Update Account.jsx

--- react-client/src/components/Account.jsx
+++ react-client/src/components/Account.jsx
@@ -1,42 +1,66 @@
-import React, {useContext} from 'react';
-import {Navigate} from 'react-router-dom';
-import {Context} from '../firebase/Context';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { Context } from '../firebase/Context';
 
 function Account() {
-    const {currentUser} = useContext(Context);
-    if (currentUser) {
-        return (
-          <div>
-            <h2>
-                Account page -- TODO: show user account details
-            </h2>
-            <ul>
-                <li>
-                <p>For renters - list all bookmarked apartments (with links) + all their reviews </p>
-                </li>
-                <li>
-                <p>Add delete button to each review in the list</p>
-                </li>
-                <li>
-                <p>For landlords - list all their building listings (with links) + options to edit/delete the listings </p>
-                </li>
-                <li>
-                <p>For admins - list all submitted applications for building listings + user reviews </p>
-                </li>
-                <li>
-                <p>Admins can approve/deny each application </p>
-                </li>
-            </ul>
+    const { currentUser } = useContext(Context);
+    const [userData, setUserData] = useState(null);
 
-            <a href='/change-password' className='btn btn-primary'>
-            Change Password
-            </a>
-    
-          </div>
-        );
-    } else {
+    const fetchUserData = async () => {
+        if (currentUser && currentUser.id) {
+            try {
+                const response = await fetch(`http://localhost:3000/user/${currentUser.id}`);
+                const data = await response.json();
+                setUserData(data);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (currentUser) {
+            fetchUserData();
+        }
+    }, [currentUser]);
+
+    if (!currentUser) {
         return <Navigate to='/' />;
     }
+
+    const renderRenterContent = () => (
+        <div>
+            <h3>Renter Content</h3>
+            {/* Render bookmarked apartments and reviews */}
+        </div>
+    );
+
+    const renderLandlordContent = () => (
+        <div>
+            <h3>Landlord Content</h3>
+            {/* Render building listings with edit/delete options */}
+        </div>
+    );
+
+    const renderAdminContent = () => (
+        <div>
+            <h3>Admin Content</h3>
+            {/* Render applications and reviews with approve/deny options */}
+        </div>
+    );
+
+    return (
+        <div>
+            <h2>Account Page</h2>
+            {userData?.accountType === 'renter' && renderRenterContent()}
+            {userData?.accountType === 'landlord' && renderLandlordContent()}
+            {userData?.accountType === 'admin' && renderAdminContent()}
+
+            <Link to='/change-password' className='btn btn-primary'>
+                Change Password
+            </Link>
+        </div>
+    );
 }
-    
+
 export default Account;
