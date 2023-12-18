39ca9a4a3deb1f4ace1676cf836bd2f416d4d65b
George Sheng <gsheng112@gmail.com>
2023-12-17 22:55:39 -0500

    Fixed bug in import statement

--- server/src/data/reviews.js
+++ server/src/data/reviews.js
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import { apartments, reviews } from "./../configs/mongoCollection.js";
-import { apartmentFunctions } from "./apartments.js";
+import * as apartmentFunctions from "./apartments.js";
 import helpers from './../utils/helpers.js';
 
 export const createReview = async(posterId, apartmentId, rating, content,
