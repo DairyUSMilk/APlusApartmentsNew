f7017116a880403e4d9a282005a82644d5e632ae
TriLuu01 <tluu@stevens.edu>
2023-12-21 14:15:49 -0500

    minor fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
-export default = {
-  apps : [{
-    name: "apollo-server",
-    script: "server.js",
-  },
-  {
-    name: "redis",
-    script: "redis-server",
-    args: "/etc/redis/redis.conf"
-    
-  }]
+export default {
+  apps: [
+    {
+      name: "apollo-server",
+      script: "server.js",
+    },
+    {
+      name: "redis",
+      script: "redis-server",
+      args: "/etc/redis/redis.conf",
+    },
+  ],
 };
