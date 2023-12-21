bcc73aa9dc88439f8e1e85748a7cd7ba45605a7f
Guizhi Xu <113871493+GarethTorres@users.noreply.github.com>
2023-12-20 22:06:19 -0500

    Finished SignIn.jsx

--- react-client/src/components/SignIn.jsx
+++ react-client/src/components/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Navigate, useNavigate} from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 import {UserContext} from '../context/UserContext';
 import {logIn, googleLogIn} from '../firebase/AuthFunctions';
 import '../index.css';
@@ -14,7 +14,6 @@ export const googleSignIn = async () => {
 
 function SignIn() {
   const {currentUser} = useContext(UserContext);
-  const navigate = useNavigate();
   
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -22,67 +21,61 @@ function SignIn() {
 
     try {
       await logIn(email.value, password.value);
-      navigate('/');
     } catch (error) {
       alert(error);
     }
   };
 
+  console.log(currentUser)
   if (currentUser) {
+    console.log('here')
     return <Navigate to='/' />;
   }
   return (
-    <div>
-      <div className='card'>
-        <h1>Log In</h1>
+      <div className='form card'>
+        
+        <div className="card_header">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+          <path fill="none" d="M0 0h24v24H0z"></path>
+          <path fill="currentColor" d="M4 15h2v5h12V4H6v5H4V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6zm6-4V8l5 4-5 4v-3H2v-2h8z"></path>
+        </svg>
+        <h1 className="form_heading">Sign In</h1>
+      </div>
+
         <form className='form' onSubmit={handleLogin}>
-          <div className='form-group'>
-            <label>
-              Email Address:
+          <div className='field'>
+              <label htmlFor="email">
+              Email:
               <br />
-              <input
-                name='email'
-                id='email'
-                type='email'
-                placeholder='Email'
-                required
-                autoFocus={true}
-              />
+              <input className="input" required name="email" type="email" placeholder="Email" id="email" />
             </label>
           </div>
-          <br />
-          <div className='form-group'>
-            <label>
+          <div className='field'>
+              <label htmlFor="password">
               Password:
               <br />
-              <input
-                name='password'
-                type='password'
-                placeholder='Password'
-                autoComplete='off'
-                required
-              />
+              <input className="input" id="password" name="password" type="password" placeholder="Password" autoComplete="off" required />
             </label>
           </div>
+          <br/>
 
-          <button className='button' style={{ verticalAlign: 'middle' }} type='submit'>
+          <div className="buttons-container">
+
+          <button className='button-sign' type='submit'>
           <span>Log in</span>
           </button>
-        </form>
 
-        <a href='/forgot-password' className='btn btn-primary'>
-            Forgot Password
+        <a href='/forgot-password' className='button-forget' >
+          <span>Forgot Password</span>
         </a>
 
-        <br />
-        <div>
-          <button className='button' style={{ verticalAlign: 'middle' }} onClick={() => googleSignIn()}>
+          <button className='button-sign' onClick={() => googleSignIn()}>
           <span>Sign in</span>
           </button>
-        </div>
     
+        </div>
+      </form>
       </div>
-    </div>
   );
 }
 
