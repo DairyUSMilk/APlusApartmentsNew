8829925cbca29e5cc4a82f8f367279ffac47024f
George Sheng <gsheng112@gmail.com>
2023-12-21 15:27:31 -0500

    added renter details

---
+++ react-client/src/components/RenterDetails.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, useState } from "react";
+import { UserContext } from "../context/UserContext";
+import { useMutation } from "@apollo/client";
+import { editRenter } from "../graphql/Mutations";
+import EditRenterModal from "./EditRenterModal";
+
+const RenterDetails = () => {
+    const [isModalVisible, setModalVisibility] = useState(false);
+    const {userData, accountType} = useContext(UserContext);
+    const [updateRenter, {loading, error}] = useMutation(editRenter());
+
+    const showModal = () => {
+        setModalVisibility(true);
+    };
+
+    const hideModal = () => {
+        setModalVisibility(false);
+    }
+
+    const updateRenterInfo = (formData) => {
+        formData.id = userData.id;
+        formData.dateOfBirth = helpers.reformatDateForDatabaseCall(formData.dateOfBirth);
+        try{
+            updateRenter({variables: formData})
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    return (
+        <div className="card">
+            <div className="user">
+              <h2>Renter Details</h2>
+              <h3>{userData.name}</h3>
+              <p>Born: {userData.dateOfBirth}</p>
+              <p>{userData.gender}</p>
+            </div>
+            <button onClick={showModal}>Edit Account Info</button>
+            <EditRenterModal
+                isOpen={isModalVisible}
+                closeModal={hideModal}
+                callDatabaseFunction={updateRenterInfo}
+                userData={userData}
+            /> 
+        </div>
+    )
+}
+
+export default RenterDetails;
